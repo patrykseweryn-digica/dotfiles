20dca8b50333f2fa940665ef04b5cd3f7c46bf42
Patryk Seweryn <patryk.seweryn@digica.com>
2025-11-05 10:21:14 +0100

    Add aliases for common shell shortcuts and Git commands

---
+++ aliases.sh
@@ -0,0 +1,40 @@
+# Shortcuts
+alias copyssh="xclip -selection clipboard < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="exec $SHELL"
+alias reloaddns="sudo systemd-resolve --flush-caches"
+alias ll="ls -AhlF --color=auto --group-directories-first"
+alias shrug="echo '¯\_(ツ)_/¯' | wl-copy"
+alias timestamp="date +%s"
+alias version="commit 'version'"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/.local/share"
+alias projects="cd $HOME/apps"
+alias sites="cd $HOME/Herd"
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run dev"
+
+# Docker
+alias docker-composer="docker compose"
+
+# Git
+alias gs="git status"
+alias gb="git branch --sort=-committerdate"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias force="git push --force-with-lease"
+alias nuke="git clean -df && git reset --hard"
+alias pop="git stash pop"
+alias prune="git fetch --prune"
+alias pull="git pull"
+alias push="git push"
+alias resolve="git add . && git commit --no-edit"
+alias stash="git stash -u"
+alias unstage="git restore --staged ."
+alias wip="commit wip"
